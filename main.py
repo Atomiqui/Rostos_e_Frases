5a297afbbe5a026399f13a3f5bcb1843669204dd
Atomiqui <acschmidt@inf.ufsm.br>
2024-03-26 10:25:14 -0300

    Revert "Update funcionalities"
    This reverts commit 00aa90864713bf94826af354d8d26b597421d703.

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import cv2
+
 import Functionalities as func
+from Functionalities import face_info
+
+
 
 mp_face_mesh = func.mp.solutions.face_mesh
 
 # Tentativa de acesso à camera com tratamento se der erro (except)
 try:
     cap = cv2.VideoCapture(0)
-    if cap.isOpened():
+    if not cap.isOpened():
+        raise Exception("Não foi possível abrir a câmera.")
+    else:
         cap.set(3, 4096)  # Largura da captura
         cap.set(4, 4096)  # Altura da captura
-    else:
-        raise Exception("Não foi possível abrir a câmera.")
 except Exception as e:
     print(f"Erro ao abrir a câmera: {e}")
 
+i = random.randint(0, 2)
+face_detected = False
+image_folder = "images"
+image_copy = None
 with open("cont.txt", "r") as file:
     cont = int(file.read().strip())
-    
+face_info = [None, None, None]
+
 with mp_face_mesh.FaceMesh(
     max_num_faces=3,
     refine_landmarks=True,
@@ -26,7 +35,7 @@ with mp_face_mesh.FaceMesh(
     cv2.namedWindow('MediaPipe FaceMesh', cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty('MediaPipe FaceMesh', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-    func.main_loop(cap, face_mesh, cont)
+    func.main_loop(cap, face_mesh)
         
 cap.release()
 cv2.destroyAllWindows()
